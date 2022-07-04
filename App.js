2940da85326d4088b7113a6aeccd1834500b6a48
Gark Godwin Duque <garkgodwinduque@gmail.com>
2022-07-04 20:54:29 +0800

    init app

--- App.js
+++ App.js
@@ -1,20 +1,88 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+  ScrollView,
+} from "react-native";
 
 export default function App() {
+  const [inputTodo, setInputTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const todoInputHandler = (enteredText) => {
+    setInputTodo(enteredText);
+  };
+
+  const addTodoHandler = () => {
+    setTodos((currentTodos) => [...currentTodos, inputTodo]);
+    //reset todo text input
+    setInputTodo("");
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.textInput}
+          placeholder="What do you want to do?"
+          value={inputTodo}
+          onChangeText={todoInputHandler}
+        />
+        <Button title="Add this" onPress={addTodoHandler} />
+      </View>
+      <View style={styles.todosContainer}>
+        <ScrollView>
+          {todos.map((todo, index) => (
+            <View style={styles.todoItemContainer} key={index}>
+              <Text style={styles.todoItemText}>{todo}</Text>
+            </View>
+          ))}
+        </ScrollView>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
+    paddingTop: 56,
+    paddingHorizontal: 16,
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    width: "100%",
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    flex: 1,
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "#cccccc",
+    width: "70%",
+    marginRight: 8,
+  },
+  todosContainer: {
+    flex: 11,
+    marginTop: 24,
+    borderColor: "transparent",
+    borderTopColor: "#cccccc",
+    borderWidth: 1,
+  },
+  todoItemContainer: {
+    backgroundColor: "#3a3b3c",
+    justifyContent: "center",
+    height: 40,
+    fontSize: 16,
+    marginTop: 4,
+    borderRadius: 8,
+    padding: 4,
+    paddingHorizontal: 8,
+  },
+  todoItemText: {
+    color: "white",
   },
 });
